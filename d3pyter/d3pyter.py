a82cd06b5fc4208ac45388245249501a140322ca
Will Sorenson <ryan.d.sorenson@gmail.com>
2015-12-03 13:24:32 -0500

    basic stem of the file

---
+++ d3pyter/d3pyter.py
@@ -0,0 +1,47 @@
+"""
+
+"""
+from odo.utils import copydoc # TODO: Change this so it doesn't require
+from .utils import docstring_copy
+__all__ = ['d3', 'sigma']
+
+default_css = ''
+
+class base_javascript(object):
+    """
+    Base class for
+    """
+
+    def __init__(self, css=default_css, text, data):
+        self.css = css
+        self.text = text
+        self.data = data
+
+
+
+class D3(base_javascript):
+    """
+
+    examples
+    ---
+    >>> from d3pyter import d3
+
+    """
+
+
+@docstring_copy(D3)
+def d3():
+
+
+
+class Sigma(base_javascript):
+    """
+
+    """
+
+@docstring_copy(Sigma)
+def sigma():
+    """
+
+    :return:
+    """
